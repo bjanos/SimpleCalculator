f16893990437c569865c1c0b906c73a1c7c15102
eajennb <janos.benyovszki@ericsson.com>
2018-10-08 15:37:06 +0100

    add icon

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.util.ResourceBundle;
@@ -25,6 +26,7 @@ public class Main extends Application {
         primaryStage.setTitle("Simple Calculator");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
+        primaryStage.getIcons().add(new Image("res/icon/icon.png"));
         primaryStage.show();
     }
 
