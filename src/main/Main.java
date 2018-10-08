d636580f0340db99aade2b41379278aa845fd0cb
bjanos <jimi.bj.86@gmail.com>
2018-10-08 15:39:21 +0100

    update

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.util.ResourceBundle;
@@ -26,7 +25,6 @@ public class Main extends Application {
         primaryStage.setTitle("Simple Calculator");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
-        primaryStage.getIcons().add(new Image("res/icon/icon.png"));
         primaryStage.show();
     }
 
